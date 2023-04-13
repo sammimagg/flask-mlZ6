be56a30af1907a8db9ffd80ae2876b324208f22f
Samúel Magnússon <39244791+sammimagg@users.noreply.github.com>
2023-04-13 21:05:57 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
 from flask import Flask, jsonify
+from flask import Flask, request, jsonify
+from transformers import pipeline
 import os
+
 
 app = Flask(__name__)
 
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
+@app.route('/predict', methods=['POST'])
+def predict():
+    text = request.json['text']
+    classifier = pipeline('text-classification', model='model')
+    result = classifier(text)
+    label = result[0]['label']
+    score = result[0]['score']
+    response = {'label': label, 'score': score}
+    return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(port=5000, debug=True)
