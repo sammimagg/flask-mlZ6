6d317d79eaa3ed22e1a229927aadaebd3ead78cc
Samúel Magnússon <39244791+sammimagg@users.noreply.github.com>
2023-04-13 22:57:55 +0000

    test

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-from flask import Flask, jsonify
 from flask import Flask, request, jsonify
-from transformers import pipeline
+from transformers import pipeline, AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
 import os
+
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
+
+config = AutoConfig.from_pretrained('./model')
+tokenizer = AutoTokenizer.from_pretrained('./model')
+model = AutoModelForSequenceClassification.from_pretrained('./model', config=config)
+
+classifier = pipeline('text-classification', model=model, tokenizer=tokenizer)
 
 app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.json['text']
-    classifier = pipeline('text-classification', model='model')
     result = classifier(text)
     label = result[0]['label']
     score = result[0]['score']
