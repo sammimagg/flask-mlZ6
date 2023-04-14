41cd1de3658daa33dd1f51112437ef59f984f13c
sammimagg <sammimagg@gmail.com>
2023-04-14 00:01:08 +0000

    test nr 3

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from transformers import pipeline, AutoConfig, AutoModelForSequenceClassification, AutoTokenizer
 import os
-
+#from flask_cors import CORS
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 
 config = AutoConfig.from_pretrained('./model')
@@ -11,7 +11,7 @@ model = AutoModelForSequenceClassification.from_pretrained('./model', config=con
 classifier = pipeline('text-classification', model=model, tokenizer=tokenizer)
 
 app = Flask(__name__)
-
+#CORS(app)
 @app.route('/predict', methods=['POST'])
 def predict():
     text = request.json['text']
@@ -22,4 +22,4 @@ def predict():
     return jsonify(response)
 
 if __name__ == '__main__':
-    app.run(port=5000, debug=True)
+    app.run(debug=True, host='0.0.0.0')
